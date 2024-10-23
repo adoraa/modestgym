43ae90a62fe70898c970aa518c84fbe3ada0885b
Adora <adoraonyile@gmail.com>
2024-10-23 02:46:03 +0100

    removed base url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/adoraa.github.io/modestgym' : './',
-  };
+export default defineConfig({
+  plugins: [react()],
 })
