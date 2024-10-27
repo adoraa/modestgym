cdf2d0190da3900c87d2e3bfdcbce3a5bc22666e
Adora <adoraonyile@gmail.com>
2024-10-27 12:27:55 +0100

    profile icon + dropdown

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { HiMiniBars3CenterLeft } from 'react-icons/hi2'
 import { IoSearchOutline } from 'react-icons/io5'
 import { HiOutlineUser } from 'react-icons/hi2'
 import { HiOutlineHeart } from 'react-icons/hi2'
 import { HiOutlineShoppingCart } from 'react-icons/hi2'
+import avatar from '../assets/avatar.png'
+
+const navigation = [
+  {name: "Dashboard", href:"/dashboard"},
+  {name: "Orders", href:"/order"},
+  {name: "Cart", href:"/cart"},
+  {name: "Checkout", href:"/checkout"}
+]
 
 export const Navbar = () => {
 
-  const currentUser = false;
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+  const currentUser = true;
   return (
     <header className='max-w-screen-2xl mx-auto px-4 py-6'>
         <nav className='flex justify-between items-center'>
@@ -31,9 +41,30 @@ export const Navbar = () => {
               <div>
                 {
                   currentUser ? <>
-                  <button>
-                    <img src="" alt="" />
+                  <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                    <img src={avatar} alt="user-profile-image" className={`size-7 rounded-full ${
+                      currentUser ? 'ring-2 ring-blue-500' : ''}`} />
                   </button>
+                  {/* show dropdown */}
+                  {
+                    isDropdownOpen && (
+                      <div className='absolute right-0 mt-2 w-48 bg-white
+                      shadow-lg rounded-md z-40'>
+                        <ul className='py-2'>
+                          {
+                            navigation.map((item) => (
+                              <li key={item.name}>
+                                <Link to={item.href} className='block
+                                px-4 py-2 text-sm hover:bg-gray-100'>
+                                  {item.name}
+                                </Link>
+                              </li>
+                            ))
+                          }
+                        </ul>
+                      </div>
+                    )
+                  }
                   </> : <Link to='/login'><HiOutlineUser className='size-6'/></Link>
                 }
               </div>
