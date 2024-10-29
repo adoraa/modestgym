bfc86e15d4af5fbd8136beafb3c0b85a7173c62d
Adora <adoraonyile@gmail.com>
2024-10-29 08:56:19 +0100

    slide js responsive breakpoints - slider

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,4 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { BookCard } from "../books/BookCard";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const categories = [
   "choose a genre",
@@ -6,7 +17,7 @@ const categories = [
   "Fiction",
   "Horror",
   "Adventure",
-]
+];
 
 export const TopSellers = () => {
   const [books, setBooks] = useState([]);
@@ -18,11 +29,14 @@ export const TopSellers = () => {
       .then((data) => setBooks(data));
   }, []);
 
-  const filteredBooks = selectedCategory === "choose a genre" ? books : books.filter(book => 
-    book.category === selectedCategory.toLowerCase()
-  )
+  const filteredBooks =
+    selectedCategory === "choose a genre"
+      ? books
+      : books.filter(
+          (book) => book.category === selectedCategory.toLowerCase()
+        );
 
-  console.log(filteredBooks)
+  console.log(filteredBooks);
 
   return (
     <div className="py-10">
@@ -30,7 +44,7 @@ export const TopSellers = () => {
       {/* category filtering */}
       <div className="mb-8 flex items-center">
         <select
-        onChange={(e) => setSelectedCategory(e.target.value)}
+          onChange={(e) => setSelectedCategory(e.target.value)}
           name="category"
           id="category"
           className="border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none"
@@ -42,6 +56,37 @@ export const TopSellers = () => {
           ))}
         </select>
       </div>
+
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {filteredBooks.length > 0 && filteredBooks.map((book, index) => (
+          <SwiperSlide key={index}>
+            <BookCard book={book} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
